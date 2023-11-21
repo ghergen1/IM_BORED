022a4fcf3ee82b2fcc379607e4ed9fc869331b03
Grant Hergenroeder <ghergen1@terpmail.umd.edu>
2023-11-21 18:14:18 -0500

    added buttons and form for ideas

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-// You can add JavaScript code here if needed
-console.log('JavaScript loaded!');
+
+// Function to add a topic to the list
+var numTopics = 0;
+
+function addTopic() {
+    const topicInput = document.getElementById('topic');
+    const topicList = document.getElementById('topicList');
+
+    if (topicInput.value.trim() !== '') {
+        if (numTopics >= 5) {
+            alert('You have reached the maximum number of topics!');
+            return;
+
+        } else {
+            numTopics ++;
+            const li = document.createElement('li');
+            li.textContent = topicInput.value;
+            topicList.appendChild(li);
+
+            // Clear the input field after adding a topic
+            topicInput.value = '';
+        }
+    }
+}
+
+function clearForm() {
+    const topicList = document.getElementById('topicList');
+
+    topicList.innerHTML = '';
+
+    numTopics = 0;
+}
