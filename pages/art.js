d5c062590dc7f83d0b3c755c62ecdc9b000bbff1
Grant Hergenroeder <104812509+ghergen1@users.noreply.github.com>
2023-11-22 13:46:26 -0500

    Update about.js

--- pages/art.js
+++ pages/art.js
@@ -1,42 +1,24 @@
-import { useState } from "react"
+import React from "react";
 
-export default function MyPage() {
-  const [prompt, setPrompt] = useState("")
-  const [answer, setAnswer] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
-
-  async function handleSubmit(e) {
-    e.preventDefault()
-    setIsLoading(true)
-
-    const response = await fetch("/api/get-painting", {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ prompt: prompt })
-    })
-    const data = await response.json()
-    setAnswer(data.text)
-    setIsLoading(false)
-  }
-
-  function handleChange(e) {
-    setPrompt(e.target.value)
-  }
-
+export default function AboutPage() {
   return (
     <div className="container">
-      <h1>What Do You Want a Happy Painting Of?</h1>
-      <form className="our-form" onSubmit={handleSubmit}>
-        <input className="prompt-field" type="text" onChange={handleChange} />
-        <button className="prompt-button">Go!</button>
-      </form>
-
-      {isLoading && <div className="loading-spinner"></div>}
-
-      {isLoading == false && <img src={answer} />}
+      <h1>About IMBORED.TECH</h1>
+      <p>
+        Hi there! I'm Grant Hergenroeder, the creator of IMBORED.TECH. Welcome to a world of endless possibilities and creative inspiration!
+      </p>
+      <p>
+        IMBORED.TECH is not just a website; it's a portal to a universe of exciting ideas and activities. The mission is simple: to banish boredom and spark creativity. Life is too short to be bored, and this site is here to ensure that you never run out of things to do.
+      </p>
+      <p>
+        Whether you're looking for a fun activity for yourself, planning a group event, or just seeking some inspiration, IMBORED.TECH is your go-to AI-powered companion. The possibilities are as vast as your imagination, and the AI is ready to generate unique and exciting suggestions tailored to your preferences.
+      </p>
+      <p>
+        So, go ahead, explore the site, fill out the form, and let the magic happen. Get ready to embark on a journey of discovery and turn every moment into an opportunity for joy and creativity!
+      </p>
+      <p>
+        Thank you for being part of the IMBORED.TECH community. Your adventure begins now!
+      </p>
     </div>
-  )
+  );
 }
